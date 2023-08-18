1fe6e4f040057ee4a8c2f6dba077899c204d2c79
Mirazsarker18 <md.mirazsarker18051994@gmail.com>
2023-08-18 23:59:03 +0600

    more changes

--- js/script.js
+++ js/script.js
@@ -4,26 +4,30 @@ function calculateTriangleArea(){
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height');
     const area = 0.5 * base * height;
-    setElementInnerText('triangle-area', area);
+    const areaToDecimal = area.toFixed(2);
+    setElementInnerText('triangle-area', areaToDecimal);
 }
 
 function calculateRectangleArea() {
     const width = getInputValue('rectangle-width');
     const length = getInputValue('rectangle-length');
     const area = width * length;
-    setElementInnerText('rectangle-area', area);
+    const areaToDecimal = area.toFixed(2);
+    setElementInnerText('rectangle-area', areaToDecimal);
 }
 
 function calculateParallelogramArea(){
    const base = getInputValue('parallelogram-base');
    const height = getInputValue('parallelogram-height');
    const area = base * height;
-   setElementInnerText('parallelogram-area', area);
+   const areaToDecimal = area.toFixed(2);
+   setElementInnerText('parallelogram-area', areaToDecimal);
 }
 
 function calculateEllipseArea(){
     const large = getInputValue('ellipse-major');
     const short = getInputValue('ellipse-minor');
     const area = Math.PI * large * short;
-    setElementInnerText('ellipse-area', area);
+    const areaToDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area', areaToDecimal);
 }
